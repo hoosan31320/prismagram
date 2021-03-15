c0225b994fd4c1bfd66b2fdba7b290688a4a1f8d
hoosan31320 <hoos31320@gmail.com>
2021-03-16 00:28:26 +0700

    createAccount Mutation part Two

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,13 +4,23 @@ export default {
     Mutation: {
         createAccount: async (_, args) => {
             const { username, email, firstName = "", lastName ="", bio =""} = args;
-            return prisma.createUser({
+            const exists = await prisma.$exists.user({ 
+                OR: [
+                    { username },
+                    { email }
+                ]   
+            });
+            if (exists) {
+                throw Error("This username / email is already taken");
+            }
+            await prisma.createUser({
                 username,
                 email,
                 firstName,
                 lastName,
                 bio
             });
+            return true;
         }
     }
 };
